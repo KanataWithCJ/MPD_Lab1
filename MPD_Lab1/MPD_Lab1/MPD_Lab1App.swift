284e3e943102df69756210aa21c47747f38163fd
KanataWithCJ <1312518723@qq.com>
2023-03-15 15:17:00 +0800

    Create project

---
+++ MPD_Lab1/MPD_Lab1/MPD_Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  MPD_Lab1App.swift
+//  MPD_Lab1
+//
+//  Created by 从径源 on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct MPD_Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
